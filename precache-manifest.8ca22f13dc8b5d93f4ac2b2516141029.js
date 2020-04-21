419a80a4331ee3f6713600540f17439dbe9bd6c7
FenrirD <fenrir4504@gmail.com>
2020-04-21 15:27:33 +0900

    Updates

---
+++ precache-manifest.8ca22f13dc8b5d93f4ac2b2516141029.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "799f6113bb8ae188d4eb11e810e9c113",
+    "url": "/Test01/index.html"
+  },
+  {
+    "revision": "eff4bd97606578deb252",
+    "url": "/Test01/static/css/main.19ef28de.chunk.css"
+  },
+  {
+    "revision": "3b2bfba75e3110193ef0",
+    "url": "/Test01/static/js/2.1be62bf2.chunk.js"
+  },
+  {
+    "revision": "2f107ea67c4cbde081728ea242da3cc3",
+    "url": "/Test01/static/js/2.1be62bf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff4bd97606578deb252",
+    "url": "/Test01/static/js/main.a72fec80.chunk.js"
+  },
+  {
+    "revision": "c9f5d215e7e873f9c10e",
+    "url": "/Test01/static/js/runtime-main.0e7e8dfd.js"
+  }
+]);
